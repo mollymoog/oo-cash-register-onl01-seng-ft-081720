a2cb7bd46d9798061f1f78946f576f2a8346c12e
molly mk <mmkover@gmail.com>
2020-08-31 01:52:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   end
   
   def add_item (title, price, quantity = 1)
-    quantity.each do
+    quantity.each do |item|
       @items << title
     end
     @total += (quantity * price)

c5b7cf51844559f5ad80683f1b58300d1cbbce9d
molly mk <mmkover@gmail.com>
2020-08-31 01:48:47 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,34 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :total, :discount
+  
+  def initialize (discount = 0) 
+    @total = 0 
+    @discount = discount
+    @items = []
+  end
+  
+  def add_item (title, price, quantity = 1)
+    @total += (quantity * price)
+    while quantity > 0 do
+      @items << title
+    end
+    @total
+  end
+
+  def apply_discount
+    discount = (@total * @discount) / 100
+    if @discount > 0
+      @total -= discount
+      "After the discount, the total comes to $#{@total}."
+    else
+      "There is no discount to apply."
+    end
+  end
+  
+  def items
+    @items
+  end
+  
+end
